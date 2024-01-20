e078b6288dc0933eb77347703b09dfe4ca86dfd7
Tyrien Gilpin <gilpinedu@gmail.com>
2024-01-19 19:05:55 -0500

    services and repository config

--- src/main/java/com/tyrien/movies/controllers/MovieController.java
+++ src/main/java/com/tyrien/movies/controllers/MovieController.java
@@ -1,19 +1,24 @@
 package com.tyrien.movies.controllers;
 
+import com.tyrien.movies.domain.Movie;
 import com.tyrien.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<String> getMovies(){
-        return new ResponseEntity<String>( "All movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getMovies(){
+        return new ResponseEntity<List<Movie>>( movieService.getMovies(), HttpStatus.OK);
     }
 }
